942c1092cb1cd4e9afe904ded42d7e005df7e7f3
Joy Wanjuhi <joywanjuhi@gmail.com>
2022-06-08 14:51:13 +0300

    commit tree 9671b124ab04f68d8a9703dc356ccb1ca652b5e9

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(callback){
+    return callback();
+}
+function returnsANamedFunction(){
+     return namedFunction;
+}
+function namedFunction(){
+    console.log("")
+
+}
+function returnsAnAnonymousFunction(){
+    return function (){
+console.log("")
+
+    }
+    
+}
+
